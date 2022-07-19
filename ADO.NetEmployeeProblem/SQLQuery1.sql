9f7dbd0d061e91e69f03d811551108020486e246
shreegowri <gaganandh@gmail.com>
2022-07-19 14:21:35 +0530

    [shreegowri]ability to crteate the DB of employee pay roll and to connect that DB with the C sharp Program

---
+++ ADO.NetEmployeeProblem/SQLQuery1.sql
@@ -0,0 +1,25 @@
+﻿CREATE DATABASE EmployeePayRollProblem
+
+Create table EmployeeTable
+(
+EmployeeID int Primary Key,
+Name varchar(200),
+Department varchar(300),
+Address varchar(200),
+Phone int,
+BasicPay float,
+Gender char(1),
+StartDate varchar(200),
+TaxablePay float,
+NetPay float,
+IncomTax Float,
+Deductions float);
+
+select * from EmployeeTable;
+
+Insert into EmployeeTable values(001,'Shree','HR','1st Cross',1345678909,2000000,'F','2016-04-01',50000,800000,9000,7000),
+(002,'Gowri','Development','2st Cross',1305678909,3000000,'F','2017-05-01',60000,900000,9500,7900),
+(003,'Mahesh','CEO','3st Cross',1349678909,4000000,'M','2008-04-01',800000,900000,91000,7100);
+
+
+
