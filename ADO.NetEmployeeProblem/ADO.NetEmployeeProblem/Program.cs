294820ebb7a1462e3f9e29f829d672060da4465c
shreegowri <gaganandh@gmail.com>
2022-07-21 08:20:54 +0530

    [shreegowri]Inserting into the two tables using TSQL

--- ADO.NetEmployeeProblem/ADO.NetEmployeeProblem/Program.cs
+++ ADO.NetEmployeeProblem/ADO.NetEmployeeProblem/Program.cs
@@ -57,9 +57,19 @@ namespace ADO.NetEmployeeProblem
                     break;
                 case 5:
                     model.Name = "Gagana";
+                    model.Department = "HR";
+                    model.Address = "3rd main";
+                    model.Phone = 1234565432;
+                    model.BasicPay = 3000000;
+                    model.StartDate = "2004-09-08";
                     model.Gender = "F";
-                    model.Address = "Bengaluru";
-                    employeeRepo.InsertIntoTwoTablesusingTSQL(model);
+                    model.TaxablePay = 79000;
+                    model.NetPay = 6588;
+                    model.IncomTax = 5676;
+                    model.Deductions = 2345;
+                    employeeRepo.AddEmployee(model);
+                    //so to add the information we will use the addmethod so thats y i am commenting it
+                    //employeeRepo.InsertIntoTwoTablesusingTSQL(model);
                     employeeRepo.GetAllEmployees();
                     break;
 
